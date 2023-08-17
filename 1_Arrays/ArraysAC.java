0439ad1a292d8c743625b54801367ce1c17b464a
Taqiyy Faiz <113523139+Taqiyyfaiz@users.noreply.github.com>
2023-08-17 21:27:45 +0530

    Add files via upload

---
+++ 1_Arrays/ArraysAC.java
@@ -0,0 +1,22 @@
+package DSA.ARRAYS;
+
+public class ArraysAC {
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE; 
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) { //profit
+                int profit = prices[i] - buyPrice; //Todays Profit
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
